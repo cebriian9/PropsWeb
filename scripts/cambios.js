8309ef9d22883a6acf029012ecc97067d41c0ed8
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2023-03-01 10:13:55 +0100

    inicio terminado

--- scripts/cambios.js
+++ scripts/cambios.js
@@ -1,3 +1,5 @@
+//--Cambio de hamborgesa a X menu desplegable-----
+
 let tongleNav = false
 function perro() {
     if (tongleNav) {
@@ -10,4 +12,16 @@ function perro() {
 }
 
 
+//--correccion footer inicio de sesion-----
+
+
+function footerFix() {
+    
+    if (screen.width<995) {
+        document.getElementById("footerFix").classList.remove("fixed-bottom");
+         
+    }else{
+        document.getElementById("footerFix").classList.add("fixed-bottom");
+    }
+}
 
