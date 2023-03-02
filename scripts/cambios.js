3a1746636d7ce2cfd737cc07d101b1aef3750ea0
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2023-03-02 12:09:42 +0100

    falta fuuter

--- scripts/cambios.js
+++ scripts/cambios.js
@@ -49,5 +49,28 @@ function openTabs(evt, ajuste) {
     evt.currentTarget.className += " active";
   }
 
+
+  //--tabs en ajustess----
+
+function openTabsRegistro(evt, ajuste) {
+    
+    let i, tabContent, tabLinks;
+  
+    // escondemos todos los contenidos
+    tabContent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+  
+    // Quitamos la clase "active" de los links
+    tabLinks = document.getElementsByClassName("tabLinks");
+    for (i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+  
+    // mostramos el contenido del link pinchado
+    document.getElementById(ajuste).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
   
 
